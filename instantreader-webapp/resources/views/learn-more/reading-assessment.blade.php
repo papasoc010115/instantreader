fe3fc0b754b63f6adab2534d3fbfd331e2aaf23b
redhizon <rmhizon@up.edu.ph>
2022-07-12 16:25:12 +0800

    Add reading assessment page

---
+++ instantreader-webapp/resources/views/learn-more/reading-assessment.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.marketing-layout')
+
+@section('content')
+<!--PAGE CONTENT START--><!-- Banner -->
+<section class="page-title cursor-light">
+    <!-- Pattern Layers -->
+    <div class="pattern-layers">
+        <div class="layer-one"></div>
+        <div class="layer-two"></div>
+    </div>
+
+    <div class="auto-container">
+        <h2 class="hide-cursor">Online Reading Assessment</h2>
+        <ul class="page-breadcrumb link">
+            <li>
+                <a href="javascript:void(0)" id="scroll-to-form">
+                    <i class="icon fa-solid fa-brain"></i> Book Now <i class="fa-solid fa-angles-down" style="font-size:0.8em"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<!-- About the assessment -->
+<section class="pb-0" id="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 wow fadeInLeft">
+                <div class="heading-area">
+                    <span class="sub-title">Know what program fits.</span>
+                    <h2 class="title">Each child has a different level of <span class="alt-color js-rotating">skill</span></h2>
+                    <p class="para">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Adipiscing diam donec adipiscing tristique risus nec feugiat. Rhoncus urna neque viverra justo nec ultrices dui sapien. 
+                        Egestas sed tempus urna et pharetra pharetra massa massa ultricies. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. 
+                                            
+                    </p>
+                    <a class="btn btn-large btn-rounded btn-pink btn-hvr-blue mt-3" href="#">                    
+                        Learn More
+                        <div class="btn-hvr-setting">
+                            <ul class="btn-hvr-setting-inner">
+                                <li class="btn-hvr-effect"></li>
+                                <li class="btn-hvr-effect"></li>
+                                <li class="btn-hvr-effect"></li>
+                                <li class="btn-hvr-effect"></li>
+                            </ul>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeInRight">
+                <div class="half-img mt-5 pt-4 mt-lg-0 pt-lg-0">
+                    <img alt="vector" src="{{ asset('marketing-site/assets/img/reading-assessment-img.jpg') }}">
+                </div>
+            </div>
+        </div>
+    </div>    
+</section>
+<!--About the assessment End-->
+
+<!-- Booking Form -->
+<section id="book-reading-assessment-form" style=
+    "
+    background-color: #562FB7;
+    margin-top: 3em;
+    padding: 1em
+    "
+>
+    <div class="container">
+        <h2 style="color: white; margin-bottom: 1em">
+            Booking Form
+        </h2>
+        <form class="contact-form">
+            @csrf
+            <div class="row">
+                <!--Left Column-->
+                <div class="col-md-6">                    
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Parent's First Name" required="true">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Student's First Name" required="true">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Contact No." required="true">
+                    </div>
+                </div>
+
+                <!--Right Column-->
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <input class="form-control" type="email" placeholder="Parent's Last Name" required="true">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Student's Last Name" required="true">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Email" required="true">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="sel1" style="color: white">Select a schedule that fits you</label>
+                    <select class="form-control" id="sel1">
+                        <option>mm/dd/yyyy | 9:00AM</option>
+                        <option>mm/dd/yyyy | 9:00AM</option>
+                        <option>mm/dd/yyyy | 9:00AM</option>
+                        <option>mm/dd/yyyy | 9:00AM</option>
+                    </select>
+                </div>
+            </div>     
+
+            <div class="row">
+                <div class="col">
+                    <button class="btn btn-large btn-rounded btn-pink btn-hvr-blue mt-3">                    
+                        Submit
+                        <div class="btn-hvr-setting">
+                            <ul class="btn-hvr-setting-inner">
+                                <li class="btn-hvr-effect"></li>
+                                <li class="btn-hvr-effect"></li>
+                                <li class="btn-hvr-effect"></li>
+                                <li class="btn-hvr-effect"></li>
+                            </ul>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+
+<!-- script for scrolling down -->
+<script>
+    document.getElementById("scroll-to-form").onclick = function () {
+        document.getElementById("book-reading-assessment-form").scrollIntoView({behavior: 'smooth'});
+    };
+</script>
+<!--PAGE CONTENT END-->
+@endsection
--- instantreader-webapp/routes/web.php
+++ instantreader-webapp/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/learn-more/reading-assessment', function () {
+    return view('learn-more.reading-assessment');
+})->name('learn-more.reading-assessment');
