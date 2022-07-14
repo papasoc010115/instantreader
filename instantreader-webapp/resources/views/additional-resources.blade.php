6476ef0cfec930764cdb58667eaeb731a8fb8ab4
srnml <sclee4@up.edu.ph>
2022-07-14 09:40:20 +0800

    Add pagination for e-books/pdf section and change tags used for blogs section

--- instantreader-webapp/resources/views/additional-resources.blade.php
+++ instantreader-webapp/resources/views/additional-resources.blade.php
@@ -46,7 +46,7 @@
             <div class="card">
                 <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title pt-3">Future of websites</h5>
+                    <h5 class="card-title pt-3">Digital Marketing</h5>
                     <p class="card-text">David Villas</p>
                 </div>
                 <div class="card-footer">
@@ -68,6 +68,18 @@
                 </div>
             </div>
         </div>
+        <div class="row pt-4">
+            <div class="col-sm-12">
+                <!--Pagination-->
+                <ul class="pagination justify-content-center top55 mb-4 mb-md-0 mb-sm-3" style="padding-top=5%">
+                    <li class="page-item"><a class="page-link disabled" href="#."><i class="fa fa-angle-left"></i></a></li>
+                    <li class="page-item active"><a class="page-link" href="#.">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#.">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#.">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#."><i class="fa fa-angle-right"></i></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </section>
 <!--E-books/PDFs End-->
@@ -84,50 +96,38 @@
             </div>
         </div>
         <!--Row-->
-        <div class="row wow fadeInUp">
-            <!--Blog Item-->
-            <div class="col-lg-4">
-                <div class="news-item">
-                    <img alt="image" class="news-img" src="{{ asset('marketing-site/assets/agency/img/blog-news-1.jpg') }}">
-                    <div class="news-text-box">
-                        <span class="date main-color">October 29, 2020</span>
-                        <a href="agency/blog-list.html"><h4 class="news-title">Web design is fun</h4></a>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum dolor sit am...</p>
-                        <span class="author d-flex align-items-center" href="javascript:void(0);">
-                            <img alt="image" class="author-img bg-blue" src="{{ asset('marketing-site/assets/agency/img/avatar-1.png') }}">
-                            <h5 class="author-name">Hena Sword</h5>
-                        </span>
-                    </div>
+        <div class="card-deck wow fadeInUp">
+            <div class="card">
+                <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-1.jpg')  }}" alt="Card image cap">
+                <div class="card-body">
+                    <a href="agency/blog-list.html" class="card-text">Hena Sword</a>
+                    <a href="agency/blog-list.html"><h5 class="pt-2">Future of websites</h5></a>
+                    <p class="para">Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum dolor sit am...</p>
+                </div>
+                <div class="card-footer">
+                    <span class="date main-color">October 29, 2020</span>
                 </div>
             </div>
-            <!--Blog Item-->
-            <div class="col-lg-4">
-                <div class="news-item">
-                    <img alt="image" class="news-img" src="{{ asset('marketing-site/assets/agency/img/blog-news-2.jpg') }}">
-                    <div class="news-text-box">
-                        <span class="date main-color">October 29, 2020</span>
-                        <a href="agency/blog-list.html"><h4 class="news-title">Future of websites</h4></a>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum dolor sit am...</p>
-                        <span class="author d-flex align-items-center" href="javascript:void(0);">
-                            <img alt="image" class="author-img bg-purple" src="{{ asset('marketing-site/assets/agency/img/avatar-2.png') }}">
-                            <h5 class="author-name">David Villas</h5>
-                        </span>
-                    </div>
+            <div class="card">
+                <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="Card image cap">
+                <div class="card-body">
+                    <a href="agency/blog-list.html" class="card-text">David Villas</a>
+                    <a href="agency/blog-list.html"><h5 class="pt-2">Web design is fun</h5></a>
+                    <p class="para">Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum dolor sit am...</p>
+                </div>
+                <div class="card-footer">
+                    <span class="date main-color">October 29, 2020</span>
                 </div>
             </div>
-            <!--Blog Item-->
-            <div class="col-lg-4">
-                <div class="news-item">
-                    <img alt="image" class="news-img" src="{{ asset('marketing-site/assets/agency/img/blog-news-3.jpg') }}">
-                    <div class="news-text-box">
-                        <span class="date main-color">October 29, 2020</span>
-                        <a href="javascript:void(0);"><h4 class="news-title">Digital marketing</h4></a>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum dolor sit am...</p>
-                        <span class="author d-flex align-items-center" href="javascript:void(0);">
-                            <img alt="image" class="author-img bg-pink" src="{{ asset('marketing-site/assets/agency/img/avatar-5.png') }}">
-                            <h5 class="author-name">Jhon Walker</h5>
-                        </span>
-                    </div>
+            <div class="card">
+                <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-3.jpg')  }}" alt="Card image cap">
+                <div class="card-body">
+                    <a href="agency/blog-list.html" class="card-text">Jhon Walker</a>
+                    <a href="agency/blog-list.html"><h5 class="pt-2">Lorem ipsum dolor sit amet consectetur</h5></a>
+                    <p class="para">Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum dolor sit am...</p>
+                </div>
+                <div class="card-footer">
+                    <span class="date main-color">October 29, 2020</span>
                 </div>
             </div>
         </div>
