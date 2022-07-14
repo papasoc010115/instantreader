80d1ed19c00294f8821bf11daa859d81fceffbd9
redhizon <rmhizon@up.edu.ph>
2022-07-14 09:11:38 +0800

    Remove script

--- instantreader-webapp/resources/views/learn-more/kids-club.blade.php
+++ instantreader-webapp/resources/views/learn-more/kids-club.blade.php
@@ -169,17 +169,5 @@
         </div>
     </div>
 </section>
-
-
-<script>
-    // hide shapes from template
-    try{
-        document.getElementById("shape-left").style.visibility = "hidden";
-        document.getElementById("shape-right").style.visibility = "hidden";
-        document.getElementById("full-menu-1").style.visibility = "hidden";
-    }catch(e){
-        
-    }
-</script>
 <!--PAGE CONTENT END -->
 @endsection
