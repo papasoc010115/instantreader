d495d4d3aa94f4b9ba1dbd0051adc161bdf60ea1
redhizon <rmhizon@up.edu.ph>
2022-07-18 15:16:41 +0800

    Change font-family of crm site

--- instantreader-webapp/resources/views/app.blade.php
+++ instantreader-webapp/resources/views/app.blade.php
@@ -5,12 +5,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>ReadRight CRM</title>
+        <!-- Font Family -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('favicon.ico') }}">
         <!-- Icons -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.css">
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <style>
+            body {
+                font-family: 'Sora', sans-serif;
+            }
+        </style>
         <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <!-- Scripts -->
