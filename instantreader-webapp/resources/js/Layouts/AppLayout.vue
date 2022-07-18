a219cc8b6348f976137c3fce4e2ab4d8e6200bf4
redhizon <rmhizon@up.edu.ph>
2022-07-18 09:54:52 +0800

    Modify test files

--- instantreader-webapp/resources/js/Layouts/AppLayout.vue
+++ instantreader-webapp/resources/js/Layouts/AppLayout.vue
@@ -1,7 +1,74 @@
-<template></template>
+<template>
+    <div>
+        <!-- NAVIGATION -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="d-flex flex-grow-1">
+                <span class="w-100 d-lg-none d-block">
+                    <!-- hidden spacer to center brand on mobile --></span
+                >
+                <a class="navbar-brand d-none d-lg-inline-block" href="#">
+                    SPA Test
+                </a>
+                <a
+                    class="navbar-brand-two mx-auto d-lg-none d-inline-block"
+                    href="#"
+                >
+                    <img src="//placehold.it/40?text=LOGO" alt="logo" />
+                </a>
+                <div class="w-100 text-right">
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#myNavbar"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </div>
+            <div
+                class="collapse navbar-collapse flex-grow-1 text-right"
+                id="myNavbar"
+            >
+                <ul class="navbar-nav ms-auto flex-nowrap">
+                    <li class="nav-item">
+                        <Link
+                            href="test"
+                            class="nav-link m-2 menu-item nav-active"
+                            :class="{ active: $page.url === '/test' }"
+                            >Page 1</Link
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <Link
+                            href="test2"
+                            class="nav-link m-2 menu-item"
+                            :class="{ active: $page.url === '/test2' }"
+                            >Page 2</Link
+                        >
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- END OF NAVIGATION -->
+        <section>
+            <slot />
+        </section>
+    </div>
+</template>
 
 <script>
-export default {};
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        Link,
+    },
+};
 </script>
 
-<style></style>
+<style scoped>
+nav {
+    padding: 0.5rem 2rem !important;
+}
+</style>
--- instantreader-webapp/resources/js/Pages/Test2.vue
+++ instantreader-webapp/resources/js/Pages/Test2.vue
@@ -1,53 +1,58 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <center><h1 class="display-1">ReadRight CRM Page 2</h1></center>
+    <AppLayout>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <center>
+                        <h1 class="display-1">ReadRight CRM Page 2</h1>
+                    </center>
+                </div>
             </div>
-        </div>
-        <div class="row" style="margin-top: 20px">
-            <div class="col-md-12">
-                <center>
-                    <p>
-                        A Single Page Application Template by
-                        <strong>
-                            <i>UPLB Interns</i><i class="fas fa-heart"></i
-                        ></strong>
-                    </p>
-                </center>
+            <div class="row" style="margin-top: 20px">
+                <div class="col-md-12">
+                    <center>
+                        <p>
+                            A Single Page Application Template by
+                            <strong>
+                                <i>UPLB Interns</i><i class="fas fa-heart"></i
+                            ></strong>
+                        </p>
+                    </center>
+                </div>
             </div>
-        </div>
-        <br />
-        <div class="row">
-            <div class="col-md-12" style="text-align: center">
-                <button
-                    type="button"
-                    @click="handleClick"
-                    class="btn btn-lg btn-outline-dark"
-                >
-                    Padayon!
-                </button>
+            <br />
+            <div class="row">
+                <div class="col-md-12" style="text-align: center">
+                    <button
+                        type="button"
+                        @click="handleClick"
+                        class="btn btn-lg btn-outline-dark"
+                    >
+                        Padayon!
+                    </button>
+                </div>
             </div>
+            <br />
+            <br />
+            <center v-if="toggle">
+                <h4>Let's get it!</h4>
+            </center>
+            <br />
+            <br />
+            <center>
+                <Link href="test">Test SPA Route</Link>
+            </center>
         </div>
-        <br />
-        <br />
-        <center v-if="toggle">
-            <h4>Let's get it!</h4>
-        </center>
-        <br />
-        <br />
-        <center>
-            <Link href="test">Test SPA Route</Link>
-        </center>
-    </div>
+    </AppLayout>
 </template>
 
 <script>
 import { Link } from "@inertiajs/inertia-vue3";
-
+import AppLayout from "../Layouts/AppLayout.vue";
 export default {
     components: {
         Link,
+        AppLayout,
     },
     data() {
         return {
