85352a61bfb694c4156af8b0f1779b5c829d948d
redhizon <rmhizon@up.edu.ph>
2022-07-18 15:55:07 +0800

    Add a Test.vue file for testing

---
+++ instantreader-webapp/resources/js/Pages/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppLayout> </AppLayout>
+</template>
+
+<script>
+import AppLayout from "../Layouts/AppLayout.vue";
+export default {
+    components: {
+        AppLayout,
+    },
+};
+</script>
+
+<style></style>
