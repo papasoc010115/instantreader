2a14f4e78a1a6d8fc57e8916f6aa01234aa5a1b9
redhizon <rmhizon@up.edu.ph>
2022-07-19 14:35:05 +0800

    Modernize scrollbar

--- instantreader-webapp/resources/views/app.blade.php
+++ instantreader-webapp/resources/views/app.blade.php
@@ -21,6 +21,18 @@
                 margin: 0;
                 padding: 0;          
             }
+
+            body::-webkit-scrollbar {
+                width: 0.25rem;
+            }
+
+            body::-webkit-scrollbar-track {
+                background: white;
+            }
+
+            body::-webkit-scrollbar-thumb {
+                background: #4b4b4a;
+            }
         </style>
         <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
