7d5f6503df9d8533305afc77698b56b31344573f
srnml <sclee4@up.edu.ph>
2022-07-20 13:21:36 +0800

    Initial Sales Activity Log page

---
+++ instantreader-webapp/resources/js/Pages/SalesActivityLog.vue
@@ -0,0 +1,131 @@
+<!-- Template by: ecdcabalda -->
+<!-- Used by: sclee -->
+
+<template>
+    <AppLayout>
+        <div class="container-fluid">
+            <div class="pt-5 pb-5">
+                <!-- Header and Search Bar -->
+                <div class="d-flex">
+                    <div>
+                        <p class="h2 text-gray-900 mb-4">SALES ACTIVITY LOG</p>
+                    </div>
+                    <div class="ml-auto">
+                        <form
+                            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
+                        >
+                            <div class="input-group">
+                                <input
+                                    type="text"
+                                    class="form-control bg-light border-0 small"
+                                    placeholder="Search for..."
+                                    aria-label="Search"
+                                    aria-describedby="basic-addon2"
+                                />
+                                <div class="input-group-append">
+                                    <button
+                                        class="btn btn-primary"
+                                        type="button"
+                                    >
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <!-- Main Table -->
+                <div class="table-responsive mt-5">
+                    <table
+                        class="table table-striped table-hover text-center"
+                        id="salesActivityLogTable"
+                        width="100%"
+                        cellspacing="0"
+                    >
+                        <thead>
+                            <tr class="text-gray-900">
+                                <th>Type</th>
+                                <th>Amount</th>
+                                <th>Date</th>
+                                <th class="text-left">Remarks</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-gray-900" style="vertical-align: middle">
+                            <tr v-for="index in 8" :key="index">
+                                <td class="align-middle">Receive</td>
+                                <td class="align-middle">100.00</td>
+                                <td class="align-middle">July 11, 2022</td>
+                                <td class="align-middle text-justify">IR Kids Club Registration</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <!-- Pagination -->
+                <div class="d-flex pt-3">
+                    <div class="ml-auto">
+                        <nav class="text-center">
+                            <ul class="pagination">
+                                <li class="page-item disabled">
+                                    <Link
+                                        class="page-link"
+                                        href="#"
+                                        aria-label="Previous"
+                                    >
+                                        <span aria-hidden="true">&laquo;</span>
+                                        <span class="sr-only">Previous</span>
+                                    </Link>
+                                </li>
+                                <li class="page-item active">
+                                    <Link class="page-link" href="#">1</Link>
+                                </li>
+                                <li class="page-item">
+                                    <Link class="page-link" href="#">2</Link>
+                                </li>
+                                <li class="page-item">
+                                    <Link class="page-link" href="#">3</Link>
+                                </li>
+                                <li class="page-item">
+                                    <Link
+                                        class="page-link"
+                                        href="#"
+                                        aria-label="Next"
+                                    >
+                                        <span aria-hidden="true">&raquo;</span>
+                                        <span class="sr-only">Next</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+import AppLayout from "../Layouts/AppLayout.vue";
+
+export default {
+    components: {
+        Link,
+        AppLayout,
+    },
+    methods: {},
+};
+</script>
+
+<style scoped>
+@import "../../../public/crm-site/assets/css/sb-admin-2.css";
+.h2 {
+    font-weight: bold;
+}
+tr {
+    line-height: 50px;
+    min-height: 50px;
+    height: 50px;
+}
+</style>
