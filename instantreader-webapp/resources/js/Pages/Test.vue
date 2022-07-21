c23e408537979b08155df18c9ac9b293cbf8d4f0
redhizon <rmhizon@up.edu.ph>
2022-07-21 15:14:38 +0800

    Create a calendar component

--- instantreader-webapp/resources/js/Pages/Test.vue
+++ instantreader-webapp/resources/js/Pages/Test.vue
@@ -1,14 +1,28 @@
 <template>
-    <AppLayout
-        ><h2><strong>Page Content</strong></h2></AppLayout
-    >
+    <AppLayout>
+        <FullCalendar :options="calendarOptions" />
+    </AppLayout>
 </template>
 
 <script>
 import AppLayout from "../Layouts/AppLayout.vue";
+import "@fullcalendar/core/vdom"; // solves problem with Vite
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
 export default {
     components: {
         AppLayout,
+        FullCalendar,
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [dayGridPlugin, interactionPlugin],
+                initialView: "dayGridMonth",
+            },
+        };
     },
 };
 </script>
