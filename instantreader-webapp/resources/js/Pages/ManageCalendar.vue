f8a1c2885a8613640c5309fa16739014cab25926
redhizon <rmhizon@up.edu.ph>
2022-07-22 18:20:10 +0800

    Update ManageCalendar page

--- instantreader-webapp/resources/js/Pages/ManageCalendar.vue
+++ instantreader-webapp/resources/js/Pages/ManageCalendar.vue
@@ -5,27 +5,29 @@
             <FullCalendar :options="calendarOptions" />
             <!-- Modal -->
             <AddEventModal :show="toggle" @close="handleCloseModal" />
+            <!-- FLoating Action Button -->
+            <FloatingActionButton @press="addEvent" :show="!toggle" />
         </div>
     </AppLayout>
 </template>
 
 <script>
 import AppLayout from "../Layouts/AppLayout.vue";
+import FloatingActionButton from "@/Components/FloatingActionButton.vue";
 import "@fullcalendar/core/vdom"; // solves problem with Vite
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
-import AddEventModalVue from "@/Components/modals/AddEventModal.vue";
 import AddEventModal from "@/Components/modals/AddEventModal.vue";
 
 export default {
     components: {
         AppLayout,
-        AddEventModalVue,
         FullCalendar,
         AddEventModal,
+        FloatingActionButton,
     },
     data() {
         return {
@@ -55,9 +57,10 @@ export default {
                         end: "2022-07-01T07:30:00",
                     },
                     {
-                        title: "Deployment",
+                        title: "Website Test (Click me)",
                         start: "2022-07-06T04:30:00",
                         end: "2022-07-07T06:30:00",
+                        url: "/",
                     },
 
                     {
@@ -70,7 +73,7 @@ export default {
                         title: "Celebration",
                         start: "2022-07-09",
                         end: "2022-07-09",
-                        color: "#FF1215",
+                        color: "#fc3f3f",
                     },
                 ],
                 initialView: "dayGridMonth",
@@ -82,13 +85,16 @@ export default {
     methods: {
         // click handler for the calendar cells
         handleDateClick: function (arg) {
-            this.toggle = true; // for opening the modal
-            console.log("date click! " + arg.dateStr);
+            alert("date click! " + arg.dateStr);
         },
 
         // for closing the modal
         handleCloseModal: function () {
             this.toggle = false;
+        },
+
+        addEvent: function () {
+            this.toggle = true;
         },
     },
 };
