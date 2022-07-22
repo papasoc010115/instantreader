1dcd69ca01276b66768409917bb28946a2a38f5e
srnml <sclee4@up.edu.ph>
2022-07-22 10:53:01 +0800

    Change left and right caret icons in pagination

--- instantreader-webapp/resources/views/additional-resources.blade.php
+++ instantreader-webapp/resources/views/additional-resources.blade.php
@@ -87,11 +87,11 @@
             <div class="col-sm-12">
                 <!--Pagination-->
                 <ul class="pagination justify-content-center top55 mb-4 mb-md-0 mb-sm-3" style="padding-top=5%">
-                    <li class="page-item"><a class="page-link disabled" href="#."><i class="fa fa-angle-left"></i></a></li>
+                    <li class="page-item"><a class="page-link disabled" href="#.">&laquo;</i></a></li>
                     <li class="page-item active"><a class="page-link" href="#.">1</a></li>
                     <li class="page-item"><a class="page-link" href="#.">2</a></li>
                     <li class="page-item"><a class="page-link" href="#.">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#."><i class="fa fa-angle-right"></i></a></li>
+                    <li class="page-item"><a class="page-link" href="#.">&raquo;</i></a></li>
                 </ul>
             </div>
         </div>
@@ -156,11 +156,11 @@
             <div class="col-sm-12">
                 <!--Pagination-->
                 <ul class="pagination justify-content-center top55 mb-4 mb-md-0 mb-sm-3" style="padding-top=5%">
-                    <li class="page-item"><a class="page-link disabled" href="#."><i class="fa fa-angle-left"></i></a></li>
+                    <li class="page-item"><a class="page-link disabled" href="#.">&laquo;</i></a></li>
                     <li class="page-item active"><a class="page-link" href="#.">1</a></li>
                     <li class="page-item"><a class="page-link" href="#.">2</a></li>
                     <li class="page-item"><a class="page-link" href="#.">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#."><i class="fa fa-angle-right"></i></a></li>
+                    <li class="page-item"><a class="page-link" href="#.">&raquo;</i></a></li>
                 </ul>
             </div>
         </div>
