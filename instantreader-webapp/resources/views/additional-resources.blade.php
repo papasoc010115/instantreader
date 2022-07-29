a8a0f80ed9bfb58b3f1a7822e95aec6280267af0
srnml <sclee4@up.edu.ph>
2022-07-29 15:20:50 +0800

    Update and adjust layout

--- instantreader-webapp/resources/views/additional-resources.blade.php
+++ instantreader-webapp/resources/views/additional-resources.blade.php
@@ -7,6 +7,12 @@
     .card {
         margin-bottom: 1.5em;
     }
+    .visuals-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
 <!-- Style End -->
 
@@ -27,20 +33,76 @@
 </section>
 <!--Banner End-->
 
-<!--E-books/PDFs Start-->
+<!--Tips/Resources Library Start-->
 <section class="bg-light" id="blog">
+    <div class="auto-container px-5">
+         <!--Row of Heading-->
+         <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="heading-area mx-570 mb-md-4 mb-3 pb-2">
+                    <h2 class="title alt-color">Tips and Resources</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row of Testimonials -->
+        <div class="row text-center">
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card shadow">
+                    <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-3.jpg')  }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title pt-3">Card title</h5>
+                        <p class="card-text">"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even <span class="alt-color">slightly believable</span>. If you are going to use a passage of Lorem Ipsum."</p>
+                    </div>
+                    <div class="card-footer" style="background-color: #7004bc">
+                        <span class="text-white">Loram Ipsum</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card shadow">
+                    <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-3.jpg')  }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title pt-3">Card title</h5>
+                        <p class="card-text">"There are many variations of passages of <span class="alt-color">Lorem Ipsum available</span>, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum."</p>
+                    </div>
+                    <div class="card-footer" style="background-color: #7004bc">
+                        <span class="text-white">Lorem Ipsum</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card shadow">
+                    <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-3.jpg')  }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title pt-3">Card title</h5>
+                        <p class="card-text">"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or <span class="alt-color">randomised words</span> which don't look even slightly believable."</p>
+                    </div>
+                    <div class="card-footer" style="background-color: #7004bc">
+                        <span class="text-white">Lorem Ipsum</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!--Tips/Resources Library End-->
+
+<!--E-books/PDFs Start-->
+<section id="blog" style="background-color: #562FB7;">
     <div class="container">
         <!--Row-->
         <div class="row">
             <div class="col-md-12 text-center">
-                <div class="heading-area mx-570 pb-lg-5 mb-5">
-                    <h2 class="title mb-0">E-books and PDFs</h2>
+                <div class="heading-area mx-570 pb-md-5 mb-3">
+                    <h2 class="title mb-0 text-white">E-books and PDFs</h2>
                 </div>
             </div>
         </div>
         <!--Row-->
-        <div class="row wow fadeInUp">
-            <div class="col-lg-4 d-flex align-items-stretch">
+        <div class="row">
+            <div class="col-md-4 d-flex align-items-stretch">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-1.jpg')  }}" alt="Card image cap">
                     <div class="card-body">
@@ -54,7 +116,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="col-md-4 d-flex align-items-stretch">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="Card image cap">
                     <div class="card-body">
@@ -68,7 +130,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="col-md-4 d-flex align-items-stretch">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-3.jpg')  }}" alt="Card image cap">
                     <div class="card-body">
@@ -100,19 +162,19 @@
 <!--E-books/PDFs End-->
 
 <!--Blogs Start-->
-<section  id="blog" style="background-color: #562FB7;">
+<section  class="bg-light" id="blog">
     <div class="container">
         <!--Row-->
         <div class="row">
             <div class="col-md-12 text-center">
-                <div class="heading-area mx-570 pb-lg-5 mb-5">
-                    <h2 class="title mb-0 text-white">Blogs</h2>
+                <div class="heading-area mx-570 pb-md-5 mb-3">
+                    <h2 class="title mb-0 main-color">Blogs</h2>
                 </div>
             </div>
         </div>
         <!--Row-->
-        <div class="row wow fadeInUp">
-            <div class="col-lg-4 d-flex align-items-stretch">
+        <div class="row">
+            <div class="col-md-4 d-flex align-items-stretch">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-1.jpg')  }}" alt="Card image cap">
                     <div class="card-body">
@@ -125,7 +187,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="col-md-4 d-flex align-items-stretch">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="Card image cap">
                     <div class="card-body">
@@ -138,7 +200,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="col-md-4 d-flex align-items-stretch">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{  asset('marketing-site/assets/agency/img/blog-news-3.jpg')  }}" alt="Card image cap">
                     <div class="card-body">
@@ -167,5 +229,103 @@
     </div>
 </section>
 <!--Blogs End-->
+
+<!--Video Resources Start-->
+<section class="gradient-bg2">
+    <div class="auto-container px-5">
+        <!-- Heading -->
+        <div class="row text-center"  style="margin-bottom: 2em;">
+            <h2 class="title text-white">Video Resources</h2>
+            <p class="para text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt
+            egestas nunc, at pellentesque arcu sollicitudin et. Aliquam hendrerit diam quis ipsum
+            ultricies, quis ultricies arcu suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Fusce ut diam quis nulla faucibus venenatis. </p>
+        </div>
+
+        <!-- Row of Videos -->
+        <div class="row">
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <video id="player1" style="max-width: 100%" preload="none" controls poster="{{ asset('marketing-site/assets/agency/img/blog-news-1.jpg')}}" playsinline>
+                        <source src="{{ asset('marketing-site/assets/agency/img/video.mp4') }}" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <video id="player1" style="max-width: 100%" preload="none" controls poster="{{ asset('marketing-site/assets/agency/img/blog-news-1.jpg')}}" playsinline>
+                        <source src="{{ asset('marketing-site/assets/agency/img/video.mp4') }}" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <video id="player1" style="max-width: 100%" preload="none" controls poster="{{ asset('marketing-site/assets/agency/img/blog-news-1.jpg')}}" playsinline>
+                        <source src="{{ asset('marketing-site/assets/agency/img/video.mp4') }}" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+        </div>
+
+        <div class="row pt-4">
+            <div class="col-sm-12">
+                <!--Pagination-->
+                <ul class="pagination justify-content-center top55 mb-4 mb-md-0 mb-sm-3" style="padding-top=5%">
+                    <li class="page-item"><a class="page-link disabled" href="#.">&laquo;</a></li>
+                    <li class="page-item active"><a class="page-link" href="#.">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#.">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#.">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#.">&raquo;</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Video Resources End-->
+
+<!--Visuals Start-->
+<section class="">
+    <div class="auto-container px-5">
+        <div class="row visuals-row">
+            <div class="col-md-7">
+                <div id="visuals-carousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img class="d-block w-100" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-100" src="{{  asset('marketing-site/assets/agency/img/blog-news-2.jpg')  }}" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-5 mt-5">
+                <div class="heading-area">
+                    <h2 class="title alt-color">Lorem Ipsum</h2>
+                    <p class="para">There are many variations of passages of Lorem Ipsum available, but the majority
+                        have suffered alteration in some form, by injected humour, or randomised words which don't look
+                        even slightly believable.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Visuals End-->
 
 @endsection
