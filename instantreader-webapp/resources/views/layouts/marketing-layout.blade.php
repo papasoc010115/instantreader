51844f23f2ee7c43f9329a9e9c6b8553a6dccc92
Elroy Christian D. Cabalda <80230009+edcabalda@users.noreply.github.com>
2022-08-04 17:49:17 +0800

    Revised calendar object for reading assessment
    Main Changes:
    - changed calendar object to a carousel
    - removed scripts and stylings for FullCalendar

--- instantreader-webapp/resources/views/layouts/marketing-layout.blade.php
+++ instantreader-webapp/resources/views/layouts/marketing-layout.blade.php
@@ -23,8 +23,6 @@
     <link rel="stylesheet" href="{{ asset('marketing-site/assets/agency/css/style.css') }}" rel="stylesheet">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('marketing-site/assets/agency/css/custom-style.css') }}">
-    <!-- Calendar CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="90">
@@ -226,8 +224,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <!-- Custom script -->
 <script src="{{ asset('marketing-site/assets/agency/js/topbar.js') }}"></script>
-<!-- Calendar Script -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
 </body>
 </html>
