04a60bd5840e13633b3ed4773842d0f47593d2f7
Elroy Christian D. Cabalda <80230009+edcabalda@users.noreply.github.com>
2022-08-05 13:52:22 +0800

    Added default values and fillable property to about us

--- instantreader-webapp/database/migrations/2022_08_03_031419_create_about_us_table.php
+++ instantreader-webapp/database/migrations/2022_08_03_031419_create_about_us_table.php
@@ -65,6 +65,60 @@ return new class extends Migration
             $table->string('testimonials_para3');
             $table->string('testimonials_para4');
         });
+
+        // Insert empty values
+        DB::table('about_us')->insert(
+            array(
+                'founder_page_title' => '',
+                'founder_page_desc' => '',
+                'founder_page_keywords' => '',
+                'founder_page_author' => '',
+                'founder_teacher_vicki_title' => '',
+                'founder_teacher_vicki_para' => '',
+                'founder_teacher_vicki_image' => '',
+                'founder_ir_history_title' => '',
+                'founder_ir_history_para1' => '',
+                'founder_ir_history_para2' => '',
+                'founder_ir_history_image1' => '',
+                'founder_ir_history_image2' => '',
+                'founder_ir_history_video' => '',
+                'founder_ir_vision_title' => '',
+                'founder_ir_vision_para' => '',
+                'founder_ir_vision_image' => '',
+                'founder_ir_mission_title' => '',
+                'founder_ir_mission_para' => '',
+                'founder_ir_mission_image' => '',
+                'founder_virac_para1' => '',
+                'founder_virac_para2' => '',
+                'founder_virac_video' => '',
+                
+                'testimonials_page_title' => '',
+                'testimonials_page_desc' => '',
+                'testimonials_page_keywords' => '',
+                'testimonials_page_author' => '',
+                'parental_concerns_para1' => '',
+                'parental_concerns_para2' => '',
+                'parental_concerns_para3' => '',
+                'parental_concerns_para4' => '',
+                'parental_concerns_image1' => '',
+                'parental_concerns_image2' => '',
+                'parental_concerns_image3' => '',
+                'parental_concerns_image4' => '',
+                'parental_goals_para1' => '',
+                'parental_goals_para2' => '',
+                'parental_goals_para3' => '',
+                'parental_goals_para4' => '',
+                'parental_goals_image1' => '',
+                'parental_goals_image2' => '',
+                'parental_goals_image3' => '',
+                'parental_goals_image4' => '',
+                'testimonials_para1' => '',
+                'testimonials_para2' => '',
+                'testimonials_para3' => '',
+                'testimonials_para4' => ''
+
+            )
+        );
     }
 
     /**
