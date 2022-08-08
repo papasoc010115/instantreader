87a7692c1bf16a9a5544e0361bce7df380e07007
redhizon <rmhizon@up.edu.ph>
2022-08-08 15:46:32 +0800

    Add filter for whitespace inputs

--- instantreader-webapp/public/marketing-site/admin-panel/js/form-script.js
+++ instantreader-webapp/public/marketing-site/admin-panel/js/form-script.js
@@ -4,6 +4,38 @@
     by: rmhizon & sclee
 */
 
+const infoGetter = (el) => {
+    switch (el.dataset.fieldtype) {
+        case "big":
+            return el.value
+                .replace(/\s+/g, " ") // removes whitespaces in between words
+                .replace(/^\s+|\s+$/g, ""); // removes whitespaces at the start and at the end of the string
+
+        case "tiny":
+            return tinymce.get(el.id).getContent(); // we don't have to remove whitespaces since this is WYSIWYG
+    }
+};
+
+const isEmpty = (str, type) => {
+    if (type === "big") {
+        // remove whitespaces
+        str = str.replace(/\s/g, "");
+    } else if (type === "tiny") {
+        // remove whitespaces
+        str = str.replace(/\s/g, "");
+        str = str.replace(/<p>/g, "");
+        str = str.replace(/<\/p>/g, "");
+        str = str.replace(/\&nbsp;/g, "");
+    }
+
+    // if all characters were whitespaces
+    if (str.length === 0) {
+        return true;
+    }
+
+    return false;
+};
+
 const forms = $("form").get();
 for (let i = 0; i < forms.length; i++) {
     const form = forms[i];
@@ -13,66 +45,33 @@ for (let i = 0; i < forms.length; i++) {
         const input_element = form.children[1].children[1];
         let update_data = {
             name: input_element.name,
-            value:
-                input_element.dataset.fieldtype === "big"
-                    ? input_element.value
-                          .replace(/\s+/g, " ") // removes whitespaces in between words
-                          .replace(/^\s+|\s+$/g, "") // removes whitespaces at the start and at the end of the string
-                          .replace(/C:\\fakepath\\/g, "/marketing-site/assets/img/") //gets file name for images
-                    : tinymce.get(input_element.id).getContent(), // we don't have to remove whitespaces since this is WYSIWYG
+            value: infoGetter(input_element),
         };
-        console.log(api_route);
-        console.log(update_data);
 
-        // AJAX setup
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            },
-        });
+        if (isEmpty(update_data.value, input_element.dataset.fieldtype)) {
+            alert("Empty field not allowed!");
+        } else {
+            // AJAX setup
+            $.ajaxSetup({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                        "content"
+                    ),
+                },
+            });
 
-        // Send POST request to server
-        $.ajax({
-            url: api_route,
-            method: "POST",
-            data: update_data,
-            success: function (response) {
-                alert("Successfully Updated!");
-            },
-            error: function (error) {
-                alert("Update Failed.");
-            },
-        });
+            // Send POST request to server
+            $.ajax({
+                url: api_route,
+                method: "POST",
+                data: update_data,
+                success: function (response) {
+                    alert("Successfully Updated!");
+                },
+                error: function (error) {
+                    alert("Update Failed. Try checking character count.");
+                },
+            });
+        }
     });
 }
-
-// $("#test").on("submit", (e) => {
-//     e.preventDefault();
-//     const api_route = $("#test").data("route");
-//     const toUpdate = $("#test").data("toupdate");
-//     const update_data = {
-//         page_title: $("#title-editor").val(),
-//     };
-//     console.log(api_route);
-//     console.log(toUpdate);
-//     console.log(update_data);
-
-//     $.ajaxSetup({
-//         headers: {
-//             "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-//         },
-//     });
-
-//     // Send POST request to server
-//     $.ajax({
-//         url: api_route,
-//         method: "POST",
-//         data: update_data,
-//         success: function (response) {
-//             alert("Update Ok!");
-//         },
-//         error: function (error) {
-//             alert("Something went wrong");
-//         },
-//     });
-// });
