28f953ad6542047ce8348e08ae6177e209cc2c41
Elroy Christian D. Cabalda <80230009+edcabalda@users.noreply.github.com>
2022-08-22 17:27:02 +0800

    Renamed header for existing consultation

--- instantreader-webapp/resources/views/marketing-admin/contact-us/consultation.blade.php
+++ instantreader-webapp/resources/views/marketing-admin/contact-us/consultation.blade.php
@@ -424,7 +424,7 @@
 
             <div class="row">
                 <div class="col-lg-12">
-                    <h2>Existing Assessment Events</h2>
+                    <h2>Existing Consultation Events</h2>
                 </div>                
             </div>
 
